aa8ca5536c07baea1b7a3cb986b164d76192c8af
LentzDurand <105986828+LentzDurand@users.noreply.github.com>
2023-12-08 11:00:51 -0500

    gta 6 countdown timer

---
+++ app.js
@@ -0,0 +1,6 @@
+const days = document.querySelectorAll("days");
+const hours = document.querySelectorAll("hours");
+const minutes = document.querySelectorAll("minutes");
+const seconds = document.querySelectorAll("seconds");
+
+const newYearTime =
